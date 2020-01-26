488cc9366d9877526089713716dee3ffe4981da1
a-desna <a.sychuk@ukr.net>
2020-01-27 00:46:08 +0200

    hw_js_01_fix

---
+++ goit-js-hw-01/script/task-01.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const name = 'Генератор защитного поля';
+
+const price = 1000;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+const newPrice = price * 2;
+
+console.log(`Выбран ${name}, цена за штуку ${newPrice} кредитов`);
---
+++ goit-js-hw-01/script/task-04.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const credits = 23580;
+
+const pricePerDroid = 3000;
+
+let amountPerDroid = prompt('Введите количество!');
+
+let totalPrice;
+
+if (amountPerDroid === null) {
+  console.log('Отменено пользователем!');
+} else if ((amountPerDroid = Number.parseInt(amountPerDroid))) {
+  totalPrice = amountPerDroid * pricePerDroid;
+
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else
+    console.log(
+      `Вы купили ${amountPerDroid} дроидов, на счету осталось ${credits -
+        totalPrice} кредитов.`,
+    );
+}
