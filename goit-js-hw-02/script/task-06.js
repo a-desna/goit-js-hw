dcb248bf139dd99c8adccf56bcdb8b2d69273b97
a-desna <a.sychuk@ukr.net>
2020-02-04 19:39:55 +0200

    hw_js_02

--- goit-js-hw-02/script/task-06.js
+++ goit-js-hw-02/script/task-06.js
@@ -8,14 +8,15 @@ do {
   input = prompt('Введите число или нажмите отмена!');
   if (input === null) {
     break;
-  } else if ((input = Number(input))) {
-    numbers.push(input);
-  } else if (input !== Number) {
+  } else if (Number.isNaN(Number(input))) {
     alert('Было введено не число, попробуйте еще раз!');
+  } else if (input !== null) {
+    numbers.push(Number(input));
   }
 } while (input !== null);
 
 for (const number of numbers) {
   total += number;
 }
+// console.log(numbers);
 console.log(`Общая сумма чисел равна ${total}`);
